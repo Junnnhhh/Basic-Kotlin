acf2c02f8f07e8ea4a4a5a0480d23b801d964854
junnnhhh <imk1017@naver.com>
2023-12-31 13:37:53 +0900

    kotlin 프로그램의 흐름 제어(2023-12-31)(1)

---
+++ src/main/kotlin/chap03/section5/NormalFactorial.kt
@@ -0,0 +1,10 @@
+package chap03.section5
+
+fun main() {
+    val number = 4
+    val result: Long
+}
+
+fun factorial(n: Int): Long {
+    return if (n == 1) n.toLong() else n * factorial(n - 1)
+}
